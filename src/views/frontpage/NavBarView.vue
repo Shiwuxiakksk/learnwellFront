942a55dc0a01fbba403699cc35e2b357b8c3727b
zhw <huawzheng@gmail.com>
2023-06-26 17:26:40 +0800

    完成学生课程模块

--- src/views/frontpage/NavBarView.vue
+++ src/views/frontpage/NavBarView.vue
@@ -8,12 +8,12 @@
         background-color="#606266"
         text-color="#fff"
         active-text-color="#ffd04b">
-      <el-menu-item index="1"><a href="" target="_blank" >明学在线学习平台</a></el-menu-item>
+      <el-menu-item index="1" @click="toIndex">明学在线学习平台</el-menu-item>
       <el-menu-item index="2"><a href="" target="_blank">公告信息</a></el-menu-item>
       <el-menu-item index="3" v-if="!isLogin"  @click="toAdmin">后台管理</el-menu-item>
       <el-menu-item index="4" v-if="!isLogin" @click="toLogin">马上登陆</el-menu-item>
       <el-menu-item index="5" v-if="!isLogin" @click="toReg">点击注册</el-menu-item>
-      <el-menu-item index="3" v-if="isLogin&&!loginType"><a href="" target="_blank">课程学习</a></el-menu-item>
+      <el-menu-item index="3" v-if="isLogin&&!loginType" @click="toCourse">课程学习</el-menu-item>
       <el-menu-item index="4" v-if="isLogin&&!loginType"><a href="" target="_blank">我的作业</a></el-menu-item>
       <el-menu-item index="5" v-if="isLogin&&!loginType"><a href="" target="_blank">我的成绩</a></el-menu-item>
       <el-submenu index="6" v-if="isLogin&&!loginType">
@@ -48,13 +48,40 @@ export default {
   data() {
     return {
       activeIndex: '1',
-      isLogin:false,
+      isLogin:true,
       loginType:false,
     }
   },
+  created() {
+    this.setActiveIndex(); // 在组件创建时设置初始的activeIndex
+  },
+  watch: {
+    $route(to, from) {
+      this.setActiveIndex(); // 监听路由变化，更新activeIndex
+    }
+  },
   methods: {
+    setActiveIndex() {
+      const routePath = this.getCurrentRoute || this.$route.path; // 如果你使用了Vuex，使用getter获取当前路由路径；否则，使用$route.path获取当前路由路径
+      if (routePath.includes('/student/course')) {
+        this.activeIndex = '3';
+      } else if (routePath.includes('/announcement')) {
+        this.activeIndex = '2';
+      } else if (routePath.includes('/admin')) {
+        this.activeIndex = '3';
+      } else if (routePath.includes('/login')) {
+        this.activeIndex = '4';
+      } else if (routePath.includes('/register')) {
+        this.activeIndex = '5';
+      } else {
+        this.activeIndex = '1';
+      }
+    },
     handleSelect(key, keyPath) {
       console.log(key, keyPath);
+    },
+    toIndex(){
+      this.$router.push("/");
     },
     toAdmin(){
       this.$router.push("/admin");
@@ -65,6 +92,9 @@ export default {
     toLogin(){
       this.$router.push("/login");
     },
+    toCourse(){
+      this.$router.push("/student/course/100000");
+    }
   }
 }
 </script>
