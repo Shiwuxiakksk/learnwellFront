deed0608eec708cdf69ed8c8e4b7a0575fcd2d1a
Y.C.Zheng <503659582@qq.com>
2023-06-27 22:08:03 +0800

    添加了在首页右下角显示最新公告弹窗的功能

--- src/views/frontpage/IndexView.vue
+++ src/views/frontpage/IndexView.vue
@@ -2,24 +2,30 @@
   <div>
     <NavBar></NavBar>
     <el-carousel>
-      <el-carousel-item v-for="(item,idx) in imgwrap" :key="idx">
-        <el-image :src="item" ></el-image>
+      <el-carousel-item v-for="(item, idx) in imgwrap" :key="idx">
+        <el-image :src="item"></el-image>
       </el-carousel-item>
     </el-carousel>
     <h1>热门课程</h1>
     <el-row>
       <el-col :span="8" :offset="2">
         <el-card :body-style="{ padding: '0px' }">
-          <img src='http://1.15.248.74:9000/coursebucket/2023-06-25_225217.jpg' class="image">
-          <div style="padding: 14px;">
+          <img
+            src="http://1.15.248.74:9000/coursebucket/2023-06-25_225217.jpg"
+            class="image"
+          />
+          <div style="padding: 14px">
             <h3>操作系统</h3>
           </div>
         </el-card>
       </el-col>
       <el-col :span="8" :offset="2">
         <el-card :body-style="{ padding: '0px' }">
-          <img src='http://1.15.248.74:9000/coursebucket/2023-06-25_225304.jpg' class="image">
-          <div style="padding: 14px;">
+          <img
+            src="http://1.15.248.74:9000/coursebucket/2023-06-25_225304.jpg"
+            class="image"
+          />
+          <div style="padding: 14px">
             <h3>计算机网络</h3>
           </div>
         </el-card>
@@ -28,16 +34,22 @@
     <el-row>
       <el-col :span="8" :offset="2">
         <el-card :body-style="{ padding: '0px' }">
-          <img src='http://1.15.248.74:9000/coursebucket/2023-06-25_225329.jpg' class="image">
-          <div style="padding: 14px;">
+          <img
+            src="http://1.15.248.74:9000/coursebucket/2023-06-25_225329.jpg"
+            class="image"
+          />
+          <div style="padding: 14px">
             <h3>计算机组成原理</h3>
           </div>
         </el-card>
       </el-col>
       <el-col :span="8" :offset="2">
         <el-card :body-style="{ padding: '0px' }">
-          <img src='http://1.15.248.74:9000/coursebucket/Snipaste_2023-06-27_14-52-24.jpg' class="image">
-          <div style="padding: 14px;">
+          <img
+            src="http://1.15.248.74:9000/coursebucket/Snipaste_2023-06-27_14-52-24.jpg"
+            class="image"
+          />
+          <div style="padding: 14px">
             <h3>数据结构</h3>
           </div>
         </el-card>
@@ -47,33 +59,60 @@
 </template>
 
 <script>
-import NavBar from "@/views/frontpage/NavBarView.vue"
+import NavBar from "@/views/frontpage/NavBarView.vue";
+import axios from "axios";
+import { duration } from "moment";
 export default {
   name: "IndexView",
   data() {
     return {
-      activeIndex: '1',
-      isLogin:true,
-      loginType:true,
-      imgwrap:[
-        'http://1.15.248.74:9000/frontpagebucket/lunbo1.jpg',
-        'http://1.15.248.74:9000/frontpagebucket/lunbo2.jpg',
-        'http://1.15.248.74:9000/frontpagebucket/lunbo3.jpg',
-
+      activeIndex: "1",
+      isLogin: true,
+      loginType: true,
+      imgwrap: [
+        "http://1.15.248.74:9000/frontpagebucket/lunbo1.jpg",
+        "http://1.15.248.74:9000/frontpagebucket/lunbo2.jpg",
+        "http://1.15.248.74:9000/frontpagebucket/lunbo3.jpg",
       ],
-    }
+    };
   },
   methods: {
     handleSelect(key, keyPath) {
       console.log(key, keyPath);
     },
-    toAdmin(){
+    toAdmin() {
       this.$router.push("/admin");
     },
   },
-  components: { NavBar
+  mounted() {
+    axios.get("/api/announce/getEarliestGlobalAnno").then((res) => {
+      if (res.data.code == 200) {
+        console.log(res.data.data);
+        const date = new Date(res.data.data.createTime);
+        const formattedTime = `${date.getFullYear()}-${padZero(
+          date.getMonth() + 1
+        )}-${padZero(date.getDate())} ${padZero(date.getHours())}:${padZero(
+          date.getMinutes()
+        )}:${padZero(date.getSeconds())}`;
+        this.$notify({
+          title: "最新公告",
+          message: `
+          <p>${res.data.data.title}: ${res.data.data.content}</p>
+          <p style="font-size: 12px; color: #999">${formattedTime}</p>
+        `,
+          dangerouslyUseHTMLString: true,
+          duration: 0,
+          position: "bottom-right",
+        });
+      } else {
+        this.$message.error("无法获取最新公告");
+      }
+    });
   },
-
+  components: { NavBar },
+};
+function padZero(num) {
+  return num < 10 ? `0${num}` : num;
 }
 </script>
 <style>
@@ -88,7 +127,7 @@ export default {
   background-color: #99a9bf;
 }
 
-.el-carousel__item:nth-child(2n+1) {
+.el-carousel__item:nth-child(2n + 1) {
   background-color: #d3dce6;
 }
 .time {
@@ -118,6 +157,6 @@ export default {
 }
 
 .clearfix:after {
-  clear: both
+  clear: both;
 }
 </style>
